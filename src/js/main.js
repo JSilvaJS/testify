452a9d14b3cb75c8324deabe14a81c9ea2e0e6f3
Jonathan Silva <silva.jon86@gmail.com>
2017-02-28 16:21:46 -0500

    initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-function GildedRose (sellIn, quality, name) {
+  function GildedRose (sellIn, quality, name) {
   this.name = name;
   this.sellIn = sellIn;
   this.quality = quality;
@@ -9,6 +9,8 @@ GildedRose.prototype.tick = function () {
     if (this.quality > 0) {
       if (this.name != 'Sulfuras, Hand of Ragnaros') {
         this.quality = this.quality - 1;
+      } if (this.name == 'Conjured Mana Cake') { //added these two lines
+        this.quality = this.quality - 2;
       }
     }
   } else {
@@ -28,9 +30,15 @@ GildedRose.prototype.tick = function () {
       }
     }
   }
+
+
+
   if (this.name != 'Sulfuras, Hand of Ragnaros') {
     this.sellIn = this.sellIn - 1;
-  }
+  } 
+
+
+
   if (this.sellIn < 0) {
     if (this.name != 'Aged Brie') {
       if (this.name != 'Backstage passes to a TAFKAL80ETC concert') {
